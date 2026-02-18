7984eadb7dd9552bd1fddd668698c79d79be90b6
Em1yataka <lovecx20001@gmail.com>
2026-02-18 16:30:21 +0000

    Delete software/server/sigma3-server directory

--- software/server/sigma3-server/static/app.js
+++
@@ -1,3 +0,0 @@
-// Minimal client-side JS.
-// We keep the UI mostly server-rendered (simple + stable for coursework).
-// If you want auto-refresh telemetry, you could add polling here.
--- software/server/sigma3-server/tests/test_message_builders.py
+++
@@ -1,8 +0,0 @@
-from server import messages
-
-def test_vote_is_yes_no():
-    msg = messages.build_vote_command("v1", 20)
-    assert msg["type"] == "vote"
-    assert msg["payload"]["options"] == ["yes", "no"]
-    assert msg["payload"]["one_vote_per_seat"] is True
-    assert msg["payload"]["auto_close"] is True
