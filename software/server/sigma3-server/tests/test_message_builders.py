e201251bc32572d4ad98f6a0cc2d53f3dde09c0a
Em1yataka <lovecx20001@gmail.com>
2026-02-26 12:05:43 +0000

    Delete software/server directory

--- software/server/sigma3-server/tests/test_message_builders.py
+++
@@ -1,6 +0,0 @@
-from server import messages
-
-def test_vote_is_yes_no():
-    msg = messages.build_vote_command("v1", 20)
-    assert msg["type"] == "vote"
-    assert msg["payload"]["options"] == ["yes", "no"]
